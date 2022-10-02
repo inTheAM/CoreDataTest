77e1dc293f2e950a8ecd0801d977e6749e6edeea
Ahmed Mgua <mguaahmed@gmail.com>
2022-10-02 12:04:12 +0200

    Initial Commit

---
+++ CoreDataTest/CoreDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataTestApp.swift
+//  CoreDataTest
+//
+//  Created by Ahmed Mgua on 02/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
